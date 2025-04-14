4dc1db81a5fe25d292f78462f1d165df94a76be1
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 <sefikaefeoglu@gmail.com>
2025-04-14 08:45:06 +0200

    update main

--- src/models/bert/main.py
+++ src/models/bert/main.py
@@ -1,27 +1,28 @@
-from transformers import BertTokenizer, BertForSequenceClassification, BertConfig
+from transformers import BertTokenizer, BertConfig
 import torch
 from re_dataset import RelationDataset, data_preparation, read_json, write_json
 from train_bert import train_model
 from test_bert import test_model
 from baseline import BertForSequenceClassification_Neuro
-def main():
 
+def main(out_dir, dataset_path, neuro_genesis, baseline_name,  batch_list, epoch_list):
 
-    epoch_list = [1]
-    batch_list = [16]
     results = []
     # torch.cuda.empty_cache()
     for run_id in range(1,4):
+
         all_labels = []
         all_seen_test_data = []
         all_test_sentences = []
         all_test_labels = []
+
         for task_id in range(1, 11):
-            train_data = read_json(f"/content/tacred/final/train/run_1/task{task_id}/train_1.json")
+
+            train_data = read_json(f"{dataset_path}/train/run_{run_id}/task{task_id}/train_1.json")
             train_prepared = data_preparation(train_data)
-            test_data = read_json(f"/content/tacred/final/test/run_1/task{task_id}/test_1.json")
+            test_data = read_json(f"{dataset_path}/test/run_{run_id}/task{task_id}/test_1.json")
             test_prepared = data_preparation(test_data)
-            val_data = read_json(f"/content/tacred/final/train/run_1/task{task_id}/dev_1.json")
+            val_data = read_json(f"{dataset_path}/train/run_{run_id}/task{task_id}/dev_1.json")
             val_prepared = data_preparation(val_data)
             all_labels.extend([item['relation'] for item in train_prepared])
             # Create unique mapping for all labels
@@ -51,7 +52,7 @@ def main():
                 # Load the configuration first
             config = BertConfig.from_pretrained('bert-large-uncased', num_labels=len(label_to_int))
             # Then pass it to the model constructor
-            model = BertForSequenceClassification_Neuro(config, pretrained_model_name='bert-large-uncased', num_classes=len(label_to_int), use_custom_encoder=True)
+            model = BertForSequenceClassification_Neuro(config, pretrained_model_name='bert-large-uncased', num_classes=len(label_to_int), use_custom_encoder=True, neuro_genesis=neuro_genesis)
             # else:
 
             #     #The old model should have the total number of labels encountered till the last task.
@@ -64,13 +65,14 @@ def main():
 
             test_dataset = RelationDataset(test_sentences, test_labels, tokenizer, max_length=512)
             all_seen_test_data = RelationDataset(all_test_sentences, test_labels_seen, tokenizer, max_length=512)
+
             for i, epoch in enumerate(epoch_list):
 
                 for batch in batch_list:
                     model, tokenizer, train_hist = train_model(epoch, batch, val_dataset, train_dataset, model, run_id, task_id)
 
-                    write_json(train_hist, f"./neurogenesis_results_low/train_hist_{run_id}_{task_id}.json")
-                    model_name_hf = f"bert_large_baseline_neurogenesis_{run_id}_{task_id}"
+                    write_json(train_hist, f"{out_dir}/train_hist_{run_id}_{task_id}.json")
+                    model_name_hf = f"{baseline_name}_{run_id}_{task_id}"
                     # base_model_hf = f"Sefika/bert_large_baseline_{run_id}_{task_id}"
                     # model.push_to_hub(model_name_hf, private=True)
                     test_acc = test_model(model, test_dataset, batch)
@@ -79,7 +81,7 @@ def main():
                     print(f"Epoch-------------{epoch}=={i}")
                     result = {"run_id":run_id, "task_id": task_id, "epoch": epoch, "batch_size": batch, "test_acc": test_acc, "seen_test_acc":test_seen_acc}
                     results.append(result)
-                    write_json(results, f"./neurogenesis_results_low/results_{run_id}.json")
+                    write_json(results, f"{out_dir}/results_{run_id}.json")
 
                     torch.cuda.empty_cache()
                 # del model
@@ -87,4 +89,10 @@ def main():
         del model
 
 if __name__ == "__main__":
-    main()
+    output_dir = "./neurogenesis_results_low"
+    neuro_genesis = True
+    epoch_list = [1]
+    batch_list = [16]
+    baseline_name = "bert_large_baseline_neurogenesis"
+    dataset_path = "/content/tacred/final"
+    main(output_dir, dataset_path, neuro_genesis,baseline_name, epoch_list, batch_list)
